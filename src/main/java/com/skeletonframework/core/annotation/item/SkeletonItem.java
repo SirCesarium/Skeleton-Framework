a6f35b65048fb104410642e4829bf0b84987494f
SirCesarium <cesarium3600@gmail.com>
2026-01-17 23:10:03 -0400

    chore: remove SkeletonAnnotation

--- src/main/java/com/skeletonframework/core/annotation/item/SkeletonItem.java
+++ src/main/java/com/skeletonframework/core/annotation/item/SkeletonItem.java
@@ -1,13 +1,10 @@
 package com.skeletonframework.core.annotation.item;
 
-import com.skeletonframework.core.annotation.SkeletonAnnotation;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@SkeletonAnnotation
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface SkeletonItem {
