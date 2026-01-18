ef28827f6421ae29aa883b9c50e3c55d4f793b10
SirCesarium <cesarium3600@gmail.com>
2026-01-17 22:41:14 -0400

    feat(bootstrapper): create bootstrapAll method to auto-scan all mods

---
+++ src/main/java/com/skeletonframework/SkeletonFramework.java
@@ -0,0 +1,19 @@
+package com.skeletonframework;
+
+import com.skeletonframework.core.SkeletonMod;
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(SkeletonFramework.MOD_ID)
+public class SkeletonFramework {
+    public static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MOD_ID = "skeletonframework";
+
+    public SkeletonFramework(IEventBus eventBus) {
+        SkeletonMod.bootstrapAll(eventBus);
+    }
+}
--- src/main/java/com/skeletonframework/core/SkeletonMod.java
+++ src/main/java/com/skeletonframework/core/SkeletonMod.java
@@ -1,5 +1,6 @@
 package com.skeletonframework.core;
 
+import com.skeletonframework.SkeletonFramework;
 import com.skeletonframework.core.annotation.item.SkeletonItem;
 import com.skeletonframework.core.reflection.AnnotationScanHelper;
 import com.skeletonframework.core.reflection.ReflectionProcessor;
@@ -7,6 +8,7 @@ import com.skeletonframework.core.reflection.resolver.FieldResolver;
 import com.skeletonframework.core.visitor.SkeletonItemVisitor;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.ModList;
 
 import java.lang.annotation.ElementType;
 import java.lang.reflect.Field;
@@ -23,5 +25,21 @@ public class SkeletonMod {
                 scanner.find(SkeletonItem.class, ElementType.FIELD),
                 new SkeletonItemVisitor(eventBus, container)
         );
+    }
+
+    public static void bootstrapAll(IEventBus eventBus) {
+        for (ModContainer container : ModList.get().getSortedMods()) {
+            try {
+                bootstrap(eventBus, container);
+            } catch (Exception e) {
+                SkeletonFramework.LOGGER.error(
+                        "SkeletonFramework failed processing mod '{}'",
+                        container.getModId(),
+                        e
+                );
+
+                throw e;
+            }
+        }
     }
 }
