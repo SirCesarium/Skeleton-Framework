a332ade88266d6792ace4ee1197850a9cf2f3ca8
SirCesarium <cesarium3600@gmail.com>
2026-01-19 17:01:24 -0400

    test: create test blocks and items

--- src/devmod/java/com/example/devmod/DevMod.java
+++ src/devmod/java/com/example/devmod/DevMod.java
@@ -2,13 +2,32 @@ package com.example.devmod;
 
 import io.github.sircesarium.skeletonframework.core.annotation.generic.SkeletonBootstrap;
 import io.github.sircesarium.skeletonframework.core.annotation.block.SkeletonBlock;
+import io.github.sircesarium.skeletonframework.core.annotation.item.SkeletonItem;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.fml.common.Mod;
+
+import java.util.function.Supplier;
 
 @SkeletonBootstrap
 @Mod("devmod")
 public class DevMod {
     public DevMod() {}
+
     @SkeletonBlock("test_block")
     public static Block TEST_BLOCK;
+
+    @SkeletonItem("test_item")
+    public static Item TEST_ITEM;
+
+    @SkeletonBlock("test_block_with_supplier")
+    public static Supplier<Block> TEST_BLOCK_SUPPLIER = () -> new Block(BlockBehaviour.Properties.of().jumpFactor(5.0f));
+
+    @SkeletonItem("test_item_with_supplier")
+    public static Supplier<Item> TEST_ITEM_SUPPLIER = () -> new Item(new Item.Properties().food(new FoodProperties.Builder().alwaysEdible().build()));
+
+    @SkeletonBlock(value = "test_block_without_item", withItem = false)
+    public static Block TEST_BLOCK_WITHOUT_ITEM;
 }
