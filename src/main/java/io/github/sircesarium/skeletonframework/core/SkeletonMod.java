6e87beaf2838aa768db73cde1f8eb003a246dad3
SirCesarium <cesarium3600@gmail.com>
2026-01-19 17:27:46 -0400

    fix: optimize annotation scanning using AnnotationIndex

--- src/main/java/io/github/sircesarium/skeletonframework/core/SkeletonMod.java
+++ src/main/java/io/github/sircesarium/skeletonframework/core/SkeletonMod.java
@@ -4,7 +4,6 @@ import io.github.sircesarium.skeletonframework.SkeletonFramework;
 import io.github.sircesarium.skeletonframework.core.annotation.block.SkeletonBlock;
 import io.github.sircesarium.skeletonframework.core.annotation.generic.SkeletonBootstrap;
 import io.github.sircesarium.skeletonframework.core.annotation.item.SkeletonItem;
-import io.github.sircesarium.skeletonframework.core.reflection.AnnotationScanHelper;
 import io.github.sircesarium.skeletonframework.core.reflection.ReflectionProcessor;
 import io.github.sircesarium.skeletonframework.core.reflection.resolver.FieldResolver;
 import io.github.sircesarium.skeletonframework.core.visitor.SkeletonBlockVisitor;
@@ -18,9 +17,9 @@ import java.lang.reflect.Field;
 
 public class SkeletonMod {
     public static void bootstrap(IEventBus eventBus, ModContainer container) {
-        AnnotationScanHelper scanner = new AnnotationScanHelper(container);
+        AnnotationIndex index = new AnnotationIndex(container);
 
-        boolean shouldSkip = scanner.find(SkeletonBootstrap.class, ElementType.TYPE).findAny().isEmpty();
+        boolean shouldSkip = index.find(SkeletonBootstrap.class, ElementType.TYPE).findAny().isEmpty();
 
         if (shouldSkip) return;
 
@@ -29,12 +28,12 @@ public class SkeletonMod {
         ReflectionProcessor<Field> processor = new ReflectionProcessor<>(fieldResolver);
 
         processor.process(
-                scanner.find(SkeletonItem.class, ElementType.FIELD),
+                index.find(SkeletonItem.class, ElementType.FIELD),
                 new SkeletonItemVisitor(eventBus, container)
         );
 
         processor.process(
-                scanner.find(SkeletonBlock.class, ElementType.FIELD),
+                index.find(SkeletonBlock.class, ElementType.FIELD),
                 new SkeletonBlockVisitor(eventBus, container)
         );
     }
