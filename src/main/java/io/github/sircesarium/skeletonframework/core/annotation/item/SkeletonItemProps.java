4577fce37454674882ae73db4ed5925c6d8bf344
SirCesarium <cesarium3600@gmail.com>
2026-01-19 20:44:44 -0400

    feat: create @SkeletonItemProps annotation

---
+++ src/main/java/io/github/sircesarium/skeletonframework/core/annotation/item/SkeletonItemProps.java
@@ -0,0 +1,13 @@
+package io.github.sircesarium.skeletonframework.core.annotation.item;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface SkeletonItemProps {
+    String parent() default "";
+}
+
