93df659b7edc639c5ba04e4c68300d72aac77472
SirCesarium <cesarium3600@gmail.com>
2026-01-23 19:59:19 -0400

    feat: create beacon fallback exception

---
+++ src/main/java/io/github/sircesarium/beaconcore/core/error/BeaconFallbackException.java
@@ -0,0 +1,7 @@
+package io.github.sircesarium.beaconcore.core.error;
+
+public class BeaconFallbackException extends RuntimeException {
+    public BeaconFallbackException(String message) {
+        super(message);
+    }
+}
