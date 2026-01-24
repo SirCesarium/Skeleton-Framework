7f697439602e23c4ea7316b135c1aa1946f538b9
SirCesarium <cesarium3600@gmail.com>
2026-01-24 15:39:27 -0400

    feat: update property registry to use supplier

--- src/main/java/io/github/sircesarium/beaconcore/core/registry/PropertyRegistry.java
+++ src/main/java/io/github/sircesarium/beaconcore/core/registry/PropertyRegistry.java
@@ -8,24 +8,30 @@ import net.minecraft.world.item.Item;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Supplier;
 
 public class PropertyRegistry {
-    private static final Map<String, Item.Properties> ITEM_PROPS = new HashMap<>();
+    private static final Map<String, Supplier<Item.Properties>> ITEM_PROPS = new HashMap<>();
 
-    public static void saveItemProps(String name, Item.Properties props) {
+    public static void saveItemProps(String name, Supplier<Item.Properties> propsSupplier) {
         if (ITEM_PROPS.containsKey(name)) {
             throw new BeaconRegistryException(BeaconErrorFormatter.formatDuplicateProperty(name));
         }
-        ITEM_PROPS.put(name, props);
+        ITEM_PROPS.put(name, propsSupplier);
     }
 
     public static Item.Properties getItemProps(String id) {
-        Item.Properties props = ITEM_PROPS.get(id);
+        Supplier<Item.Properties> supplier = ITEM_PROPS.get(id);
+
+        if (supplier == null) {
+            String suggestion = TextUtil.findClosestMatch(id, ITEM_PROPS.keySet());
+            throw new BeaconRegistryException(BeaconErrorFormatter.formatMissingProperty(id, suggestion));
+        }
+
+        Item.Properties props = supplier.get();
 
         if (props == null) {
-            String suggestion = TextUtil.findClosestMatch(id, ITEM_PROPS.keySet());
-            String errorMessage = BeaconErrorFormatter.formatMissingProperty(id, suggestion);
-            throw new BeaconRegistryException(errorMessage);
+            throw new BeaconRegistryException("The property supplier for '" + id + "' returned null.");
         }
 
         return props;
