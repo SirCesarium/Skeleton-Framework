f4a4380702a268e413a9bad4e04bd599a7564573
SirCesarium <cesarium3600@gmail.com>
2026-01-24 15:40:57 -0400

    feat: update item props registry visitor to save props with namespace

--- src/main/java/io/github/sircesarium/beaconcore/core/visitor/item/ItemPropsRegistryVisitor.java
+++ src/main/java/io/github/sircesarium/beaconcore/core/visitor/item/ItemPropsRegistryVisitor.java
@@ -5,42 +5,57 @@ import io.github.sircesarium.beaconcore.core.error.BeaconRegistryException;
 import io.github.sircesarium.beaconcore.core.reflection.base.ReflectionVisitor;
 import io.github.sircesarium.beaconcore.core.registry.PropertyRegistry;
 import net.minecraft.world.item.Item;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.neoforgespi.language.ModFileScanData;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.function.Supplier;
 
 public final class ItemPropsRegistryVisitor implements ReflectionVisitor<Field> {
+    private final String modNamespace;
+
+    public ItemPropsRegistryVisitor(ModContainer container) {
+        this.modNamespace = container.getModId();
+    }
 
     @Override
     public void visit(Field field, ModFileScanData.AnnotationData data) {
-        String propsId = (String) data.annotationData().get("value");
-
-        if (propsId == null || propsId.isEmpty()) {
-            propsId = field.getName();
-        }
-
         validateField(field);
+
+        String propsId = (String) data.annotationData().getOrDefault("value", field.getName());
+        String namespace = (String) data.annotationData().getOrDefault("namespace", modNamespace);
 
         try {
             Object value = field.get(null);
+
             if (value instanceof Item.Properties props) {
-                PropertyRegistry.saveItemProps(propsId, props);
+                PropertyRegistry.saveItemProps(namespace + ":" + propsId, () -> props);
+            } else if (value instanceof Supplier<?> supplier) {
+                PropertyRegistry.saveItemProps(namespace + ":" + propsId, () -> {
+                    Object supplied = supplier.get();
+                    if (!(supplied instanceof Item.Properties p)) {
+                        throw new BeaconRegistryException("Supplier '" + field.getName() + "' must return Item.Properties");
+                    }
+                    return p;
+                });
             } else {
                 throw new BeaconRegistryException(
-                        "Field '" + field.getName() + "' is not Item.Properties"
+                        "Field '" + field.getName() + "' in " + field.getDeclaringClass().getSimpleName() +
+                                " must be Item.Properties or Supplier<Item.Properties>"
                 );
             }
+
         } catch (IllegalAccessException e) {
-            throw new BeaconReflectionException("Error accessing field", e);
+            throw new BeaconReflectionException("Failed to access static field: " + field.getName(), e);
         }
     }
 
     private void validateField(Field field) {
         if (!Modifier.isStatic(field.getModifiers())) {
             throw new BeaconRegistryException(
-                    "@RegisterItemProps field must be static: "
-                            + field.getDeclaringClass().getName() + "#" + field.getName()
+                    "@RegisterItemProps field must be static: " +
+                            field.getDeclaringClass().getName() + "#" + field.getName()
             );
         }
     }
