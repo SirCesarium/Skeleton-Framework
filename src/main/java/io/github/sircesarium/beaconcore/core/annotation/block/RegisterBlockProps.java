8a0b35bd9cab8e17d8fe9b337014a8cc3aca8948
SirCesarium <cesarium3600@gmail.com>
2026-01-25 16:27:10 -0400

    feat: create RegisterBlockProps annotation

---
+++ src/main/java/io/github/sircesarium/beaconcore/core/annotation/block/RegisterBlockProps.java
@@ -0,0 +1,13 @@
+package io.github.sircesarium.beaconcore.core.annotation.block;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface RegisterBlockProps {
+    String value() default "";
+    String namespace() default "";
+}
